26bdac91012286f9ef818033ef01c566b30a50f6
mrtnbrjs <jesus.martin2093@gmail.com>
2023-07-17 12:25:49 -0700

    cambios

---
+++ src/components/atoms/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export interface ButtonProps {
+	text: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ text }) => {
+	return <button>{text}</button>;
+};
+
+export default Button;

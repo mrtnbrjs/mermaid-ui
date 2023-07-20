9c2092e41b7ddfd6e4a16dc7b2a50ed5945d32d6
mrtnbrjs <jesus.martin2093@gmail.com>
2023-07-20 09:07:35 -0700

    index change

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { default as Button } from "./atoms/Button";
+export { default as Heading } from "./atoms/Heading";
+export { default as Checkbox } from "./atoms/Checkbox";
+export { default as Radio } from "./atoms/Radio";
+export { default as Spinner } from "./atoms/Spinner";
+export { default as Text } from "./atoms/Text";
+// Path: src/components/index.ts
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-export { default as Button } from "./components/atoms/Button/Button.tsx";
-export { default as Heading } from "./components/atoms/Heading/Heading.tsx";
-export { default as Checkbox } from "./components/atoms/Checkbox/Checkbox.tsx";
-export { default as Radio } from "./components/atoms/Radio/Radio.tsx";
-export { default as Spinner } from "./components/atoms/Spinner/Spinner.tsx";
-export { default as Text } from "./components/atoms/Text/Text.tsx";
-// Path: src/components/index.ts
+export * from "./components";

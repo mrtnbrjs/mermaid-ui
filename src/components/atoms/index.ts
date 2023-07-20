c23aaf8738534194be5f56b6154569e41d2defbd
mrtnbrjs <jesus.martin2093@gmail.com>
2023-07-19 21:55:04 -0700

    index change

--- src/components/atoms/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Button } from "./Button/Button";
-export { default as Heading } from "./Heading/Heading";
-export { default as Checkbox } from "./Checkbox/Checkbox";
-export { default as Radio } from "./Radio/Radio";
-export { default as Spinner } from "./Spinner/Spinner";
-export { default as Text } from "./Text/Text";

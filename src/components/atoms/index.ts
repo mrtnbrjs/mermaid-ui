4824cb4bd7518ebc0d2b5389d74a89282b969b6e
mrtnbrjs <jesus.martin2093@gmail.com>
2023-07-19 21:48:31 -0700

    readme

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,6 +1,6 @@
-export * from "./Button/Button";
-export * from "./Heading/Heading";
-export * from "./Checkbox/Checkbox";
-export * from "./Radio/Radio";
-export * from "./Spinner/Spinner";
-export * from "./Text/Text";
+export { default as Button } from "./Button/Button";
+export { default as Heading } from "./Heading/Heading";
+export { default as Checkbox } from "./Checkbox/Checkbox";
+export { default as Radio } from "./Radio/Radio";
+export { default as Spinner } from "./Spinner/Spinner";
+export { default as Text } from "./Text/Text";
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { default as Button } from "./components/atoms/Button";
+export { default as Heading } from "./components/atoms/Heading";
+export { default as Checkbox } from "./components/atoms/Checkbox";
+export { default as Radio } from "./components/atoms/Radio";
+export { default as Spinner } from "./components/atoms/Spinner";
+export { default as Text } from "./components/atoms/Text";
+// Path: src/components/index.ts

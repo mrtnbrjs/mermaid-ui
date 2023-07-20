81969adaa7ec4b6e9ff270de891366316c7d5ccd
mrtnbrjs <jesus.martin2093@gmail.com>
2023-07-19 22:11:05 -0700

    index change

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-export { default as Button } from "./components/atoms/Button";
-export { default as Heading } from "./components/atoms/Heading";
-export { default as Checkbox } from "./components/atoms/Checkbox";
-export { default as Radio } from "./components/atoms/Radio";
-export { default as Spinner } from "./components/atoms/Spinner";
-export { default as Text } from "./components/atoms/Text";
+export { default as Button } from "./components/atoms/Button/Button.tsx";
+export { default as Heading } from "./components/atoms/Heading/Heading.tsx";
+export { default as Checkbox } from "./components/atoms/Checkbox/Checkbox.tsx";
+export { default as Radio } from "./components/atoms/Radio/Radio.tsx";
+export { default as Spinner } from "./components/atoms/Spinner/Spinner.tsx";
+export { default as Text } from "./components/atoms/Text/Text.tsx";
 // Path: src/components/index.ts
